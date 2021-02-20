9f61ac49d1ffc620a1f14d9e1aa5180c4f199ffc
Morgan Collado <morgan.collado@gmail.com>
2021-02-19 18:19:19 -0600

    establishes relationship between rides and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,7 @@ class User < ApplicationRecord
     has_many :passenger_rides, :class_name => "Ride", :foreign_key => "passenger_id"
     has_many :driver_rides, :class_name => "Ride", :foreign_key => "driver_id"
     has_secure_password
-    
+    validates_uniqueness_of :email, case_sensitive: false
+    validates_presence_of :email, :name, :phone_number
+
 end
